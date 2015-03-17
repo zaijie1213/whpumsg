39dfa5a4932be0ef4a418fc7a80bb293ef8bd32d
zaijie1213 <zaijie8@gmail.com>
2015-03-17 05:09:22 -0700

    first commit

---
+++ zz/spiders/test_spider.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = 'zg'
+from scrapy.spider import BaseSpider
+
+class TestSpider(BaseSpider):
+    name="zuojia"
+    allowed_domains=["whpu.edu.cn"]
+    start_urls=[
+        "http://www.whpu.edu.cn"
+    ]
+    def parse(self,response):
+	
+        print response
