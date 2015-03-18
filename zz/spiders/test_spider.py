a2c0a32eb14cfd9ce9a164713e2b7f537f439ffa
zaijie1213 <zaijie8@gmail.com>
2015-03-18 10:00:15 +0800

    change url

--- zz/spiders/test_spider.py
+++ zz/spiders/test_spider.py
@@ -7,6 +7,9 @@ class TestSpider(BaseSpider):
     allowed_domains=["whpu.edu.cn"]
     start_urls=[
         "http://www.whpu.edu.cn"
+        "http://www.whpu.edu.cn/showmore.aspx?type=%D0%C5%CF%A2&Ctype=%D0%A3%D4%B0%B9%AB%B8%E6"
+        "http://www.whpu.edu.cn/showmore.aspx?type=%D0%C5%CF%A2&Ctype=%D1%A7%D0%A3%D0%C2%CE%C5"
+        "http://www.whpu.edu.cn/showmore.aspx?type=%D0%C5%CF%A2&Ctype=%D1%A7%CA%F5%B6%AF%CC%AC"
     ]
     def parse(self, response):
         filename = "a.txt"
